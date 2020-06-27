d8fba9368666564adfad71ac485c97114772247c
Sebastian Russo <russo.sebastian@gmail.com>
2020-06-27 16:24:02 -0400

    added directions to render on items on menu

--- src/components/your-menu-item.js
+++ src/components/your-menu-item.js
@@ -5,19 +5,24 @@ import {connect} from 'react-redux';
 export function YourMenuItem(props) {
     console.log(props)
     const {menuItems} = props;
-
+    // why the [0] if it's the only one in the array?
+    // filtered menut item obj from array of objs, by filtering params id
+    // previously, i thought i'd have to map array of obj, then map array obj again, to get to ingredients obj
     const menuItem = menuItems.filter(menuItem => menuItem.name === props.match.params.id)[0];
-    
+
     const ingredients = menuItem.ingredients.map((ingredient, i) => {
         return (
         <div key={`ingredients-${i}`}>{ingredient.amount} of {ingredient.ingredient}</div>
         )
     })
-console.log(menuItem)
+    
     return (
         <div className="menu-item">
             <h1>{menuItem.name}</h1>
+            <h3>Ingredients:</h3>
             <div>{ingredients}</div>
+            <h3>Directions: </h3>
+            <div>{menuItem.directions}</div>
         </div>
     );
 }
@@ -28,10 +33,3 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps)(YourMenuItem)
 
-{/* 
-        // <div className="menu-menuItem" key={menuItem.id}>
-        //     <h2 className="menu-menuItem-name">{menuItem.name}</h2>
-        //     <YourMenuItemIngredients />
-        //     <br></br>
-        //     <YourMenuItemSteps />
-        // </div> */}
