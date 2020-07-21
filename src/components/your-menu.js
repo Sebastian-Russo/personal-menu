27430ca7aee3066061f06af94d3180c1ff41b015
Sebastian Russo <russo.sebastian@gmail.com>
2020-07-21 09:44:01 -0400

    add styling

--- src/components/your-menu.js
+++ src/components/your-menu.js
@@ -10,7 +10,7 @@ export function YourMenu({categoryList}) {
         return (
             <div key={`categories-${i}`}>
                 <h3>
-                    <Link to={`/your-menu-category/${category}`} idName="categories-text">
+                    <Link to={`/your-menu-category/${category}`} className="category-text">
                         {category}
                     </Link>
                 </h3>
